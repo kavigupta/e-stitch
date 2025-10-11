a9b65191ee71bd620dbef1789320ee6b045addb0
Maddy Bowers <mlbowers@mit.edu>
2025-10-11 10:06:56 -0400

    tweak

--- src/pattern.rs
+++ src/pattern.rs
@@ -4,7 +4,7 @@ use crate::revexpr::RevExpr;
 
 #[derive(Debug)]
 pub struct Pattern {
-    pub pattern: egg::PatternAst<StitchLang>,
+    pub pattern: RevExpr<ENodeOrVar<StitchLang>>,
     pub vars: Vec<Id>,
     pub max_var: u32, // not same as arity because can expand away a var
 }
@@ -13,54 +13,34 @@ impl Pattern {
     /// Creates the initial #?0 pattern which is just a single var
     pub fn single_var() -> Self {
         // annoyingly parsing "#?0" doesn't create a ENodeOrVar::Var it creates an ENodeOrVar::ENode
-        let rec_expr: egg::RecExpr<ENodeOrVar<StitchLang>>  = vec![ENodeOrVar::Var(egg::Var::from(0))].into();
+        let e: RevExpr<ENodeOrVar<StitchLang>>  = RevExpr::new(vec![ENodeOrVar::Var(egg::Var::from(0))]);
         Pattern {
-            pattern: rec_expr,
+            pattern: e,
             vars: vec![0.into()],
             max_var: 0,
         }
     }
 
-    /// Inserts a node at the given Id into the pattern, and increments the ids of the nodes
-    /// of all children after the inserted node by 1
-    pub fn insert(&mut self, i: Id, node: egg::ENodeOrVar<StitchLang>) {
-        let i: usize = i.into();
-        for node in &mut self.pattern.nodes {
-            node.children_mut().iter_mut().for_each(|child| {
-                if *child >= i.into() {
-                    *child = Id::from(1 + usize::from(*child));
-                }
-            });
-        }
-        for var in &mut self.vars {
-            if *var >= i.into() {
-                *var = Id::from(1 + usize::from(*var));
-            }
-        }
-        &mut self.pattern.nodes.insert(i, node);
-    }
-
-    pub fn new_var(&mut self, i: Id) {
+    /// Creates a new variable with a fresh name and adds it to the pattern
+    pub fn new_var(&mut self) -> Id {
         self.max_var += 1;
         let arg_node = ENodeOrVar::Var(egg::Var::from(self.max_var));
-        self.insert(i, arg_node);
-        self.vars.push(i); // must happen after insert, since insert upshifts vars
+        self.pattern.nodes.push(arg_node);
+        let new_id = Id::from(self.pattern.nodes.len()-1);
+        self.vars.push(new_id);
+        new_id
     }
 
     /// Expands the pattern at the given Id with the given node
-    pub fn expand(&mut self, var_idx: usize, node: &StitchLang) {
+    pub fn expand(&mut self, var_idx: usize, target: &StitchLang) {
         let var = self.vars.remove(var_idx);
-        let i = usize::from(var);
-        let mut new_node = node.clone();
+        let mut new_node = target.clone();
         let num_vars = new_node.len();
         for j in 0..num_vars {
-            let var_id = Id::from(j);
-            self.new_var(var_id);
-            new_node.children[j] = var_id;
+            new_node.children[j] = self.new_var();
         }
-        let replace_at = usize::from(i) + num_vars;
-        assert!(matches!(self.pattern.nodes[replace_at], ENodeOrVar::Var(_)), "Attempting to expand a non-var");
-        self.pattern.nodes[replace_at] = ENodeOrVar::ENode(new_node);
+        assert!(matches!(self.pattern[var], ENodeOrVar::Var(_)), "Attempting to expand a non-var");
+        self.pattern[var] = ENodeOrVar::ENode(new_node);
     }
 }
 
--- src/revexpr.rs
+++ src/revexpr.rs
@@ -1,5 +1,7 @@
-/// Like an egg::RecExpr but with the nodes in reverse order
-/// and publicly accessible
+/// Like an egg::RecExpr but with the nodes in reverse order and publicly accessible
+/// This is much better for representing partial patterns as expanding can just
+/// append to the end of the vector, and also doesn't need to worry about shifting child Ids
+/// for nodes within the vector.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub struct RevExpr<L: egg::Language> {
     pub nodes: Vec<L>,
@@ -8,6 +10,17 @@ pub struct RevExpr<L: egg::Language> {
 impl<L: egg::Language> RevExpr<L> {
     pub fn new(nodes: Vec<L>) -> Self {
         Self { nodes }
+    }
+}
+
+/// Reverses the nodes in the vector of nodes and updates the children ids to point to the correct nodes
+fn rev_nodes<L: egg::Language>(nodes: &mut Vec<L>) {
+    nodes.reverse();
+    let max_id = nodes.len() - 1;
+    for node in nodes {
+        for child in node.children_mut() {
+            *child = egg::Id::from(max_id - usize::from(*child));
+        }
     }
 }
 
@@ -28,11 +41,24 @@ impl<L: egg::Language + std::fmt::Display> std::fmt::Display for RevExpr<L> {
     }
 }
 
+impl<L: egg::Language> std::ops::Index<egg::Id> for RevExpr<L> {
+    type Output = L;
+    fn index(&self, index: egg::Id) -> &Self::Output {
+        &self.nodes[usize::from(index)]
+    }
+}
+
+impl<L: egg::Language> std::ops::IndexMut<egg::Id> for RevExpr<L> {
+    fn index_mut(&mut self, index: egg::Id) -> &mut Self::Output {
+        &mut self.nodes[usize::from(index)]
+    }
+}
+
 
 impl<L: egg::Language> From<RevExpr<L>> for egg::RecExpr<L> {
     fn from(rev_expr: RevExpr<L>) -> Self {
         let mut nodes: Vec<L> = rev_expr.nodes;
-        nodes.reverse();
+        rev_nodes(&mut nodes);
         egg::RecExpr::from(nodes)
     }
 }
@@ -40,7 +66,7 @@ impl<L: egg::Language> From<RevExpr<L>> for egg::RecExpr<L> {
 impl<L: egg::Language> From<egg::RecExpr<L>> for RevExpr<L> {
     fn from(recexpr: egg::RecExpr<L>) -> Self {
         let mut nodes: Vec<L> = recexpr.into();
-        nodes.reverse();
+        rev_nodes(&mut nodes);
         RevExpr::new(nodes)
     }
 }
