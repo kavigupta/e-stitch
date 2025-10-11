e73642e2a5ab71364793e669b35e040d65c14bad
Maddy Bowers <mlbowers@mit.edu>
2025-10-11 07:38:06 -0400

    factor search into search.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod lang;
 mod util;
 mod pattern;
+mod search;
 
-use egg::ENodeOrVar;
 use lang::StitchLang;
-use egg::Language; // to put traits in view bc its such a pain otherwise
 use pattern::Pattern;
+use search::{SharedSearchData, SearchState};
 
 fn main() {
     let (egraph, root) = util::load_egraph::<StitchLang>("data/domains/simple-arithmetic/aplusbplusc.json");
@@ -28,35 +28,3 @@ fn main() {
 
 }
 
-
-
-pub struct SharedSearchData {
-    pub egraph: egg::EGraph<StitchLang, ()>,
-}
-
-#[derive(Debug)]
-pub struct MatchAtEClass {
-    pub root_eclass: egg::Id,
-    // variables[i][j] represents the j'th variable in the i'th way to match the pattern
-    pub variables: Vec<egg::Subst>,
-
-}
-
-#[derive(Debug)]
-pub struct SearchState {
-    pattern: Pattern,
-    // each match represents a different eclass at which `pattern` can be rooted
-    matches: Vec<MatchAtEClass>,
-}
-
-impl SearchState {
-    pub fn empty(shared: &SharedSearchData) -> Self {
-        Self {
-            pattern: Pattern::single_hole(),
-            matches: shared.egraph.classes().map(|c| MatchAtEClass {
-                root_eclass: c.id,
-                variables: vec![egg::Subst::default()],
-            }).collect(),
-        }
-    }
-}
---
+++ src/search.rs
@@ -0,0 +1,34 @@
+use crate::lang::StitchLang;
+use crate::pattern::Pattern;
+
+
+pub struct SharedSearchData {
+    pub egraph: egg::EGraph<StitchLang, ()>,
+}
+
+#[derive(Debug)]
+pub struct MatchAtEClass {
+    pub root_eclass: egg::Id,
+    // variables[i][j] represents the j'th variable in the i'th way to match the pattern
+    pub variables: Vec<egg::Subst>,
+
+}
+
+#[derive(Debug)]
+pub struct SearchState {
+    pattern: Pattern,
+    // each match represents a different eclass at which `pattern` can be rooted
+    matches: Vec<MatchAtEClass>,
+}
+
+impl SearchState {
+    pub fn empty(shared: &SharedSearchData) -> Self {
+        Self {
+            pattern: Pattern::single_hole(),
+            matches: shared.egraph.classes().map(|c| MatchAtEClass {
+                root_eclass: c.id,
+                variables: vec![egg::Subst::default()],
+            }).collect(),
+        }
+    }
+}
