3c0d0b04c48f2692e8d2e4662ea38df0f8c76770
Maddy Bowers <mlbowers@mit.edu>
2025-10-11 07:35:24 -0400

    update search state to use the new structs

--- src/main.rs
+++ src/main.rs
@@ -12,38 +12,39 @@ fn main() {
     let (_, term) = extractor.find_best(root);
     util::print_programs(&term);
 
-    let mut component = Component::single_hole();
-    println!("{}", component.pattern);
-    println!("{:?}", component.pattern.nodes);
+    let mut pattern = Pattern::single_hole();
+    println!("{}", pattern.pattern);
+    println!("{:?}", pattern.pattern.nodes);
     
 
     let recexpr: egg::RecExpr<StitchLang> = "(+ 2 3)".parse().unwrap();
     println!("{}", recexpr);
     println!("{:?}", recexpr.nodes);
 
-    component.expand(0.into(), &StitchLang{op: "+".into(), children: vec![2.into(), 3.into()]});
-    println!("{}", component.pattern);
-    println!("{:?}", component.pattern.nodes);
+    pattern.expand(0.into(), &StitchLang{op: "+".into(), children: vec![2.into(), 3.into()]});
+    println!("{}", pattern.pattern);
+    println!("{:?}", pattern.pattern.nodes);
 
 }
 
-pub struct Component {
+#[derive(Debug)]
+pub struct Pattern {
     pub pattern: egg::PatternAst<StitchLang>,
     pub holes: Vec<egg::Id>,
     pub max_var: u32, // not same as arity because can expand away a var
 }
 
-impl Component {
-    /// Creates the initial #?0 component which is just a single hole
+impl Pattern {
+    /// Creates the initial #?0 pattern which is just a single hole
     pub fn single_hole() -> Self {
-        Component {
+        Pattern {
             pattern: "#?0".parse().unwrap(),
             holes: vec![0.into()],
             max_var: 0,
         }
     }
 
-    /// Inserts a node at the given Id into the component, and increments the ids of the nodes
+    /// Inserts a node at the given Id into the pattern, and increments the ids of the nodes
     /// of all children after the inserted node by 1
     /// Assumes if you're pushing a hole, you'll do that after calling this function not before
     pub fn insert(&mut self, i: egg::Id, node: egg::ENodeOrVar<StitchLang>) {
@@ -61,7 +62,7 @@ impl Component {
         }
     }
 
-    /// Expands the component at the given Id with the given node
+    /// Expands the pattern at the given Id with the given node
     pub fn expand(&mut self, i: egg::Id, node: &StitchLang) {
         let mut new_node = node.clone();
         let num_holes = new_node.len();
@@ -80,8 +81,8 @@ impl Component {
 
 
 
-pub struct SharedSearchData<L: egg::Language> {
-    pub egraph: egg::EGraph<L, ()>,
+pub struct SharedSearchData {
+    pub egraph: egg::EGraph<StitchLang, ()>,
 }
 
 #[derive(Debug)]
@@ -93,17 +94,16 @@ pub struct MatchAtEClass {
 }
 
 #[derive(Debug)]
-pub struct SearchState<L: egg::Language> {
-    pattern: egg::PatternAst<L>,
+pub struct SearchState {
+    pattern: Pattern,
     // each match represents a different eclass at which `pattern` can be rooted
     matches: Vec<MatchAtEClass>,
 }
 
-impl <L: egg::Language + egg::FromOp> SearchState<L> {
-    pub fn empty(shared: &SharedSearchData<L>) -> Self {
-        let recexpr: egg::PatternAst<L> = "?#0".parse().unwrap();
+impl SearchState {
+    pub fn empty(shared: &SharedSearchData) -> Self {
         Self {
-            pattern: recexpr,
+            pattern: Pattern::single_hole(),
             matches: shared.egraph.classes().map(|c| MatchAtEClass {
                 root_eclass: c.id,
                 variables: vec![egg::Subst::default()],
